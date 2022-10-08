88db417de426cabf20a7ee867065fedcb0a20d75
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2022-10-08 16:36:57 -0300

    Botão de Adicionar

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,8 +1,12 @@
-import { Text, View, TextInput } from 'react-native';
+import { Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 import { styles } from './styles';
 
 export function Home() {
+  function handleParticipantAdd() {
+    console.log('Você clicou no botão de Adicionar!');
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>
@@ -18,6 +22,12 @@ export function Home() {
         placeholder="Nome do participante"
         placeholderTextColor="#6B6B6B"
       />
+
+      <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+        <Text style={styles.buttonText}>
+          +
+        </Text>
+      </TouchableOpacity>
     </View>
   )
 }
